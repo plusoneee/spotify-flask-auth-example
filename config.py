b1f706b278fcf94e37cc488a771ca34bcc5b4f7d
plusoneee <y02121148s@gmail.com>
2020-05-22 20:20:37 +0800

    'adjust'

---
+++ config.py
@@ -0,0 +1,16 @@
+#  Client Keys
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+
+# Spotify URLS
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+
+# Server-side Parameters
+CLIENT_SIDE_URL = "http://127.0.0.1"
+PORT = 8080
+REDIRECT_URI = "{}:{}/callback/".format(CLIENT_SIDE_URL, PORT)
+SCOPE = "playlist-modify-public playlist-modify-private"
--- main.py
+++ main.py
@@ -2,63 +2,42 @@ import json
 from flask import Flask, request, redirect, g, render_template
 import requests
 from urllib.parse import quote
+import config as CONFIG
 
 # Authentication Steps, paramaters, and responses are defined at https://developer.spotify.com/web-api/authorization-guide/
 # Visit this url to see all the steps, parameters, and expected response.
-
-
 app = Flask(__name__)
-
-#  Client Keys
-CLIENT_ID = ""
-CLIENT_SECRET = ""
-
-# Spotify URLS
-SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
-SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
-SPOTIFY_API_BASE_URL = "https://api.spotify.com"
-API_VERSION = "v1"
-SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
-
-# Server-side Parameters
-CLIENT_SIDE_URL = "http://127.0.0.1"
-PORT = 8080
-REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
-SCOPE = "playlist-modify-public playlist-modify-private"
-STATE = ""
-SHOW_DIALOG_bool = True
-SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
 
 auth_query_parameters = {
     "response_type": "code",
-    "redirect_uri": REDIRECT_URI,
-    "scope": SCOPE,
-    # "state": STATE,
-    # "show_dialog": SHOW_DIALOG_str,
-    "client_id": CLIENT_ID
+    "redirect_uri": CONFIG.REDIRECT_URI,
+    "scope": CONFIG.SCOPE,
+    "client_id": CONFIG.CLIENT_ID
 }
-
 
 @app.route("/")
 def index():
     # Auth Step 1: Authorization
-    url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
-    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+    query_parameters = []
+    for key, val in auth_query_parameters.items():
+        query_parameters.append("{}={}".format(key, quote(val))) 
+    url_args = "&".join(query_parameters)
+    auth_url = "{}/?{}".format(CONFIG.SPOTIFY_AUTH_URL, url_args)
     return redirect(auth_url)
 
 
-@app.route("/callback/q")
+@app.route("/callback/")
 def callback():
     # Auth Step 4: Requests refresh and access tokens
     auth_token = request.args['code']
     code_payload = {
         "grant_type": "authorization_code",
         "code": str(auth_token),
-        "redirect_uri": REDIRECT_URI,
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
+        "redirect_uri": CONFIG.REDIRECT_URI,
+        'client_id': CONFIG.CLIENT_ID,
+        'client_secret': CONFIG.CLIENT_SECRET,
     }
-    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
+    post_request = requests.post(CONFIG.SPOTIFY_TOKEN_URL, data=code_payload)
 
     # Auth Step 5: Tokens are Returned to Application
     response_data = json.loads(post_request.text)
@@ -71,7 +50,7 @@ def callback():
     authorization_header = {"Authorization": "Bearer {}".format(access_token)}
 
     # Get profile data
-    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
+    user_profile_api_endpoint = "{}/me".format(CONFIG.SPOTIFY_API_URL)
     profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
     profile_data = json.loads(profile_response.text)
 
@@ -86,4 +65,4 @@ def callback():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=PORT)
+    app.run(debug=True, port=CONFIG.PORT)
